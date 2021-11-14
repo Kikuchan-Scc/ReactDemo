c36987f9e0da4096b8c0540fe6da6576597da32e
阿梓从小就很可爱 <75628080+Kikuchan-Scc@users.noreply.github.com>
2021-11-14 18:03:30 +0800

    Update App.js
    11/14

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Book from "./view/Book";
 import Show from "./view/ShowPage";
 import Announcement from "./view/Announcement";
 import AboutUs from "./view/AboutUs";
-import NotFound from './view/NotFound'
+import NotFound from "./view/NotFound";
 import Login from "./view/Login";
 import Layout from "./view/Layout";
 import LayoutPage from "./view/LayoutPage";
@@ -13,19 +13,16 @@ import LayoutPage from "./view/LayoutPage";
 function App() {
   return (
     <Router>
-      <Layout>
-        <Route exact path="/" component={Show}></Route>
-      </Layout>
       <Switch>
         <LayoutPage>
-          <Route exact path="/Home" component={Home}></Route>
+          {/* <Route exact path="/" component={Show}></Route> */}
+          <Route exact path="/" component={Home}></Route>
           <Route exact path="/Skin" component={Book}></Route>
           <Route exact path="/Announcement" component={Announcement}></Route>
           <Route exact path="/AboutUs" component={AboutUs}></Route>
           <Route exact path="/Login" component={Login}></Route>
         </LayoutPage>
       </Switch>
-      
     </Router>
   );
 }
